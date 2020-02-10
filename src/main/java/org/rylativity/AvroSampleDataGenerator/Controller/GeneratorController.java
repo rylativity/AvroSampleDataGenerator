f3b7b9360f15ea2029ea5d21be3176c484c1e905
Ryan Stewart <ryan.stewart113@gmail.com>
2020-02-10 01:09:38 -0500

    correct return string

--- src/main/java/org/rylativity/AvroSampleDataGenerator/Controller/GeneratorController.java
+++ src/main/java/org/rylativity/AvroSampleDataGenerator/Controller/GeneratorController.java
@@ -2,15 +2,8 @@ package org.rylativity.AvroSampleDataGenerator.Controller;
 
 import org.rylativity.AvroSampleDataGenerator.Service.GeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
-import org.apache.avro.Schema;
-import org.apache.trevni.avro.RandomData;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 @RestController
 @RequestMapping()
@@ -19,8 +12,8 @@ public class GeneratorController {
     @Autowired
     GeneratorService generator;
 
-    @PostMapping("/generate")
-    String generateSampleData(@RequestBody String schema, @RequestParam int numSamples) throws IOException {
+    @PostMapping(path =  "/generate", consumes = "application/json", produces = "application/json")
+    String generateSampleData(@RequestBody String schema, @RequestParam int numSamples) {
 
         String samples = generator.generate(schema, numSamples);
         return samples;
--- src/main/java/org/rylativity/AvroSampleDataGenerator/Service/GeneratorService.java
+++ src/main/java/org/rylativity/AvroSampleDataGenerator/Service/GeneratorService.java
@@ -3,30 +3,27 @@ package org.rylativity.AvroSampleDataGenerator.Service;
 import com.google.gson.Gson;
 import org.apache.avro.Schema;
 import org.apache.trevni.avro.RandomData;
-import org.codehaus.jackson.map.ObjectMapper;
+
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 
 @Service
 public class GeneratorService {
-
-    Gson gson = new Gson();
 
     public String generate(String schema, int numSamples) {
 
         List<Object> samples = new ArrayList<>();
         Schema parsedSchema = new Schema.Parser().parse(schema);
 
-        Iterator<Object> it = new RandomData(parsedSchema, numSamples).iterator();
-        for (int i=0; i<numSamples; i++) {
-            samples.add(it.next());
+        for (Object datum : new RandomData(parsedSchema, numSamples)) {
+
+            Map<String, Object> swapMap = new HashMap<>();
+
+            samples.add(datum);
         }
 
-        return gson.toJson(samples);
+        return samples.toString().replace("\\","");
     }
 }
